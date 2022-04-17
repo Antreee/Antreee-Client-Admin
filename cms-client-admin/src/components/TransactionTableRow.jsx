6e958f753d994bacc67cee4d496dc2da9090a1f3
arda <fatikaardha@gmail.com>
2022-04-17 23:05:17 +0800

    layouting

---
+++ cms-client-admin/src/components/TransactionTableRow.jsx
@@ -0,0 +1,15 @@
+// import { Link } from "react-router-dom";
+
+export default function TransactionTableRow() {
+    return (
+      <tr className="bg-white border-b">
+        <td className="py-4 font-medium text-gray-900 whitespace-wrap"> test</td>
+        <td>test</td>
+        <td>test</td>
+        <td>test</td>
+        <td>test</td>
+        <td>test</td>
+      </tr>
+    );
+  }
+  
---
+++ cms-client-admin/src/views/DashboardPage.jsx
@@ -0,0 +1,34 @@
+// import ProductTableRow from "../components/ProductTableRow";
+
+export default function DashboardPage() {
+
+
+  // if (showCreateProduct) {
+  //   return <ProductForm changeView={changeView}>Create Product</ProductForm>;
+  // }
+
+  // if (isError) {
+  //   return (
+  //     <section className="flex flex-col w-full p-4 px-12 pt-10 md:pt-10 md:px-10 ml-52 mb-10">
+  //       <p className="p-6 text-2xl">Error....</p>
+  //     </section>
+  //   );
+  // }
+
+  // if (isLoading) {
+  //   return (
+  //     <section className="flex flex-col w-full p-4 px-12 pt-10 md:pt-10 md:px-10 ml-52 mb-10">
+  //       <p className="p-6 text-2xl">Loading....</p>
+  //     </section>
+  //   );
+  // }
+
+  return (
+    <section className="flex flex-col p-4 px-12 pt-10 md:pt-10 md:px-10 ml-96 mt-20 mb-10">
+      <div className="justify-between flex mb-6 mr-12 ml-6 items-end">
+        <h1 className="text-3xl font-semibold">Dashboard</h1>
+      </div>
+   
+    </section>
+  );
+}
